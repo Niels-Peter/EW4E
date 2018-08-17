3ba0467323613caced9a016ca218cb5385dc8a7c
Niels-Peter Rønmos <nieron@erst.dk>
2018-08-17 11:32:51 +0200

    t3

---
+++ Spain/spain_pred_from_xlsx.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 10 18:24:10 2018
+
+@author: niels-peter
+"""
+
+import numpy as np
+import pandas as pd
+from spain_transformation import *
+import pickle
+from sklearn.externals import joblib
+from numpy import math
+import os
+
+clf_EW_spain = joblib.load('/home/niels-peter/Dokumenter/EW_DK_SPAIN.pkl')
+#clf_EW_spain = joblib.load('EW_DK_SPAIN_dummy.pkl')
+
+listen = os.listdir('/home/niels-peter/Dokumenter/100_spanske_Balance_Sheets')
+
+for forekomst in listen:
+    print(forekomst)
+    df = pd.read_excel('/home/niels-peter/Dokumenter/100_spanske_Balance_Sheets/' + forekomst)
+    datalisten = df.values
+    for i in range(0, len(datalisten)):
+        if datalisten[i, 0] == 'Assets':
+            datastart = i
+            dataslut = len(datalisten[i])-1
+            break
+    dict_data = {}
+    for i in range(datastart, len(datalisten)):
+        if datalisten[i, 1] == 10000:
+            dict_data['fsa:Assets'] = datalisten[i, dataslut]
+            dict_data['fsa:Assets_prev'] = datalisten[i, dataslut-1]
+        if datalisten[i, 1] == 11000:
+            dict_data['fsa:NoncurrentAssets'] = datalisten[i, dataslut]
+            dict_data['fsa:NoncurrentAssets_prev'] = datalisten[i, dataslut-1]
+        if datalisten[i, 1] == 11100:
+            dict_data['fsa:IntangibleAssets'] = datalisten[i, dataslut]
+            dict_data['fsa:IntangibleAssets_prev'] = datalisten[i, dataslut-1]
+        if datalisten[i, 1] == 12000:
+            dict_data['fsa:CurrentAssets'] = datalisten[i, dataslut]
+            dict_data['fsa:CurrentAssets_prev'] = datalisten[i, dataslut-1]
+        if datalisten[i, 1] == 12200:
+            dict_data['fsa:Inventories'] = datalisten[i, dataslut]
+            dict_data['fsa:Inventories_prev'] = datalisten[i, dataslut-1]
+        if datalisten[i, 1] == 12300:
+            dict_data['fsa:ShorttermReceivables'] = datalisten[i, dataslut]
+            dict_data['fsa:ShorttermReceivables_prev'] = datalisten[i, dataslut-1]
+        if datalisten[i, 1] == 12700:
+            dict_data['fsa:CashAndCashEquivalents'] = datalisten[i, dataslut]
+            dict_data['fsa:CashAndCashEquivalents_prev'] = datalisten[i, dataslut-1]
+        if datalisten[i, 1] == 20000:
+            dict_data['fsa:Equity'] = datalisten[i, dataslut]
+            dict_data['fsa:Equity_prev'] = datalisten[i, dataslut-1]
+        if datalisten[i, 1] == 49500:
+            dict_data['fsa:ProfitLoss'] = datalisten[i, dataslut]
+            dict_data['fsa:ProfitLoss_prev'] = datalisten[i, dataslut-1]
+        if datalisten[i, 1] == 49100:
+            dict_data['fsa:ProfitLossFromOrdinaryOperatingActivities'] = datalisten[i, dataslut]
+            dict_data['fsa:ProfitLossFromOrdinaryOperatingActivities_prev'] = datalisten[i, dataslut-1]
+             
+    slettes = []
+    for noegle in dict_data.keys():
+        if math.isnan(dict_data[noegle]):
+            slettes.append(noegle)
+    for noegle in slettes:
+        del dict_data[noegle]   
+    print(clf_EW_spain.predict_proba([dict_data]))
---
+++ Spain/spain_transformation.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 10 18:24:10 2018
+
+@author: niels-peter
+"""
+
+import numpy as np
+import pandas as pd
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.feature_extraction import DictVectorizer
+from numpy import math
+
+class spain_to_dict(BaseEstimator, TransformerMixin):
+    """Extract features from each document for DictVectorizer"""
+
+    def fit(self, x, y=None):
+        return self
+
+    def transform(self, posts):
+        for dict in posts:            
+            aktiver = ['fsa:NoncurrentAssets', 'fsa:IntangibleAssets','fsa:PropertyPlantAndEquipment', 
+                       'fsa:LongtermInvestmentsAndReceivables', 'fsa:CurrentAssets', 'fsa:Inventories', 'fsa:ShorttermReceivables', 'fsa:ShorttermInvestments', 'fsa:CashAndCashEquivalents',
+                       'fsa:LiabilitiesOtherThanProvisions', 'fsa:LongtermLiabilitiesOtherThanProvisions', 'fsa:ShorttermLiabilitiesOtherThanProvisions',
+                       'fsa:Provisions']
+            passiver = ['fsa:Equity', 'fsa:LiabilitiesOtherThanProvisions', 'fsa:LongtermLiabilitiesOtherThanProvisions', 'fsa:ShorttermLiabilitiesOtherThanProvisions',
+                        'fsa:Provisions']            
+            if ('fsa:Assets' in dict) or ('fsa:ProfitLoss' in dict) or ('fsa:Equity' in dict):
+                for felt in ('fsa:Assets',
+                             'fsa:NoncurrentAssets',  'fsa:IntangibleAssets',  'fsa:PropertyPlantAndEquipment',  'fsa:LongtermInvestmentsAndReceivables',
+                             'fsa:CurrentAssets',  'fsa:Inventories',  'fsa:ShorttermReceivables', 'fsa:ShorttermInvestments', 'fsa:CashAndCashEquivalents',  
+                             'fsa:Equity', 'fsa:LiabilitiesOtherThanProvisions', 'fsa:LongtermLiabilitiesOtherThanProvisions', 'fsa:ShorttermLiabilitiesOtherThanProvisions', 'fsa:Provisions'):                    
+                    if felt in dict:
+                        try:
+                            dict[felt] = float(dict.get(felt, 0))
+                        except:
+                            pass
+                    if felt in aktiver:
+                        try:
+                            dict[felt + '_ratio'] = dict[felt] / dict['fsa:Assets']
+                            dict[felt + '_ratio'].replace({np.inf: 9, -np.inf: -9})
+                        except:
+                            pass
+                    if felt == 'fsa:Equity':
+                            try:
+                                dict['Soliditetsgrad'] = dict[felt] / dict['fsa:Assets']
+                                dict['Soliditetsgrad'].replace({np.inf: 9, -np.inf: -9})
+                            except:
+                                pass
+                    if felt in passiver:
+                        try:
+                            dict[felt + '_ratio'] = dict[felt] / (dict['fsa:Assets']- dict['fsa:Equity'])
+                            dict[felt + '_ratio'].replace({np.inf: 9, -np.inf: -9})
+                        except:
+                            pass
+                try:
+                    dict['egenkap_forrentning'] = dict['fsa:ProfitLoss']/float(dict['fsa:Equity'])
+                    dict['egenkap_forrentning'].replace({np.inf: 9, -np.inf: -9})
+                    if dict['egenkap_forrentning'] > 9:
+                        dict['egenkap_forrentning'] = 9
+                    if dict['egenkap_forrentning'] < -9:
+                        dict['egenkap_forrentning'] = -9
+                except:
+                    pass
+                try:
+                    dict['OrdinaryOperatingActivities_to_ProfitLoss_ratio'] = (dict['fsa:ProfitLossFromOrdinaryOperatingActivities'] - dict['fsa:ProfitLoss'])/dict['fsa:ProfitLossFromOrdinaryOperatingActivities']
+                    dict['OrdinaryOperatingActivities_to_ProfitLoss_ratio'].replace({np.inf: 9, -np.inf: -9})
+                    if dict['OrdinaryOperatingActivities_to_ProfitLoss_ratio'] > 9:
+                        dict['OrdinaryOperatingActivities_to_ProfitLoss_ratio'] = 9
+                    if dict['OrdinaryOperatingActivities_to_ProfitLoss_ratio'] < -9:
+                        dict['OrdinaryOperatingActivities_to_ProfitLoss_ratio'] = -9
+                except:
+                    pass 
+
+                        
+                if ('fsa:Assets_prev' in dict) or ('fsa:ProfitLoss_prev' in dict) or ('fsa:Equity_prev' in dict):
+                    for felt in ('fsa:Assets',
+                                 'fsa:NoncurrentAssets',  'fsa:IntangibleAssets',  'fsa:PropertyPlantAndEquipment',  'fsa:LongtermInvestmentsAndReceivables',
+                                 'fsa:CurrentAssets',  'fsa:Inventories',  'fsa:ShorttermReceivables', 'fsa:ShorttermInvestments', 'fsa:CashAndCashEquivalents',  
+                                 'fsa:Equity', 'fsa:LiabilitiesOtherThanProvisions', 'fsa:LongtermLiabilitiesOtherThanProvisions', 'fsa:ShorttermLiabilitiesOtherThanProvisions', 'fsa:Provisions'):                    
+                        if felt + '_prev' in dict:
+                            try:
+                                dict[felt + '_prev'] = float(dict.get(felt + '_prev', 0)) 
+                            except:
+                                pass
+                            try:
+                                dict[felt + '_delta'] = (dict[felt] - dict[felt + '_prev'])/dict[felt + '_prev']
+                                dict[felt + '_delta'].replace({np.inf: 9, -np.inf: -9})
+                            except:
+                                pass
+                        if felt == 'fsa:Equity':
+                            try:
+                                dict['Soliditetsgrad_prev'] = dict['fsa:Equity_prev'] / dict['fsa:Assets_prev']
+                                dict['Soliditetsgrad_prev'].replace({np.inf: 9, -np.inf: -9})
+                            except:
+                                pass
+                    try:
+                        dict['egenkap_forrentning_prev'] = dict['fsa:ProfitLoss_prev'] /float(dict['fsa:Equity_prev'])
+                        dict['egenkap_forrentning_prev'].replace({np.inf: 9, -np.inf: -9})
+                        if dict['egenkap_forrentning_prev'] > 9:
+                            dict['egenkap_forrentning_prev'] = 9
+                        if dict['egenkap_forrentning_prev'] < -9:
+                            dict['egenkap_forrentning_prev'] = -9
+                    except:
+                        pass
+                    try:
+                        dict['OrdinaryOperatingActivities_to_ProfitLoss_ratio_prev'] = (dict['fsa:ProfitLossFromOrdinaryOperatingActivities_prev'] - dict['fsa:ProfitLoss_prev'])/dict['fsa:ProfitLossFromOrdinaryOperatingActivities_prev']
+                        dict['OrdinaryOperatingActivities_to_ProfitLoss_ratio_prev'].replace({np.inf: 9, -np.inf: -9})
+                        if dict['OrdinaryOperatingActivities_to_ProfitLoss_ratio_prev'] > 9:
+                            dict['OrdinaryOperatingActivities_to_ProfitLoss_ratio_prev'] = 9
+                        if dict['OrdinaryOperatingActivities_to_ProfitLoss_ratio_prev'] < -9:
+                            dict['OrdinaryOperatingActivities_to_ProfitLoss_ratio_prev'] = -9
+                    except:
+                        pass
+
+        return [{'Assets_delta': dict.get('fsa:Assets_delta', 0),
+                 'NoncurrentAssets_delta': dict.get('fsa:NoncurrentAssets_delta', 0),
+                 'NoncurrentAssets_ratio': dict.get('fsa:NoncurrentAssets_ratio', 0),
+                 'IntangibleAssets_delta': dict.get('fsa:IntangibleAssets_delta', 0),  
+                 'IntangibleAssets_ratio': dict.get('fsa:IntangibleAssets_ratio', 0),  
+                 'CurrentAssets_delta': dict.get('fsa:CurrentAssets_delta', 0),
+                 'CurrentAssets_ratio': dict.get('fsa:CurrentAssets_ratio', 0),
+                 'Inventories_delta': dict.get('fsa:Inventories_delta', 0),
+                 'Inventories_ratio': dict.get('fsa:Inventories_ratio', 0),
+                 'ShorttermReceivables_delta': dict.get('fsa:ShorttermReceivables_delta', 0),
+                 'ShorttermReceivables_ratio': dict.get('fsa:ShorttermReceivables_ratio', 0),
+                 'CashAndCashEquivalents_delta': dict.get('fsa:CashAndCashEquivalents_delta', 0),
+                 'CashAndCashEquivalents_ratio': dict.get('fsa:CashAndCashEquivalents_ratio', 0),
+                 'Equity_delta': dict.get('fsa:Equity_delta', 0),
+                 'Solvency ratio': dict.get('Soliditetsgrad', 0),
+                 'Solvency ratio_prev': dict.get('Soliditetsgrad_prev', 0),                 
+                 'Return on Equity': dict.get('egenkap_forrentning', 0),
+                 'Return on Equity_prev': dict.get('egenkap_forrentning_prev', 0),                     
+                 'OrdinaryOperatingActivities_to_ProfitLoss_ratio': dict.get('OrdinaryOperatingActivities_to_ProfitLoss_ratio', 0),
+                 'OrdinaryOperatingActivities_to_ProfitLoss_ratio_prev': dict.get('OrdinaryOperatingActivities_to_ProfitLoss_ratio_prev', 0),
+                 } for dict in posts]
